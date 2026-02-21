a6cf8411428d467c1ff8491dbfa7ccdb4cf77076
V01d42 <89440437+V01d42@users.noreply.github.com>
2026-02-21 18:21:51 +0000

    fix: typo

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
-  input = {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts"
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = input @ {
+  outputs = inputs @ {
     nixpkgs,
     flake-parts,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      system = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
+      systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
 
       perSystem = {
         system,
@@ -19,11 +19,11 @@
         ...
       }:
       let
-        jdk = pkgs.openjdk17_headless;
+        jdk = pkgs.jdk17_headless;
 
         maven = pkgs.maven.override {
           jdk_headless = jdk;
-        }
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -34,12 +34,12 @@
               inherit jdk;
             })
 
-            (pkgs.kotlin-language-server.override) {
+            (pkgs.kotlin-language-server.override {
               openjdk = jdk;
-              inherit maven
-            }
-          ]
-        }
-      }
-    }
+              inherit maven;
+            })
+          ];
+        };
+      };
+    };
 }
